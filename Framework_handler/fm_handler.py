2634e90b5c4a4841f39f363eff27db3441e6de81
CranFused <121121675+X3r0Day@users.noreply.github.com>
2024-12-07 18:43:25 +0530

    Updated Framework

---
+++ Framework_handler/fm_handler.py
@@ -0,0 +1,27 @@
+# Framework Handler (Which will link all the pieces of framework at once place)
+import time
+import Framework_component.xss_framework
+
+
+def main():
+    while True:
+        print("Welcome to the X3r0Day Framework!")
+        time.sleep(.2)
+        opt1 = int(input("1 = XSS Scanner\n2 = SQLi Scanner\n99 = EXIT"))
+        if opt1 == 1:
+            Framework_component.xss_framework.main()
+        
+        elif opt1 == 2:
+            # Selected SQLi Scanner
+            print()
+        
+        elif opt1 == 99:
+            print("Exiting")
+            time.sleep(.5)
+            exit()
+
+        else:
+            print("Error! Please try again!")
+
+
+main()
